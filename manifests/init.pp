53e1989b46fea12d5de264f8ea8b0ec0e4819d75
Mark Greenhalgh <mgreenha@yahoo.co.uk>
2014-04-17 10:39:14 +0100

    check_mk now in repo

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,46 +10,16 @@ class check_mk (
 
   if $check_mk::install == 'agent' or $check_mk::install == 'server'{
 
-#    class{'xinetd': }
-
-    $check_mk_agent          = 'check_mk-agent-1.1.12p3-1.noarch.rpm'
     $omd_site_home           = "/opt/omd/sites/${omd_site}"
     $check_mk_location       = "${omd_site_home}/etc/check_mk"
     $check_mk_agent_location = "${check_mk_location}/agents"
 
 #    package { 'nagios-plugins-setuid': ensure => latest; }
 
-#    file { '/etc/xinetd.d/check_mk':
-#      ensure  => present,
-#      mode    => '0644',
-#      owner   => root,
-#      group   => root,
-#      require => Exec['rpm -i /tmp/$check_mk_agent'],
-#      source  => 'puppet:///check_mk/check_mk'
-#    }
-
-#    file { "/tmp/${check_mk_agent}":
-#      ensure => present,
-#      path   => "/tmp/${check_mk_agent}",
-#      source => "puppet:///check_mk/${check_mk_agent}"
-#    }
-
     #TODO: Add details for different os
     #FIXME: Move to agent.pp
     case $::osfamily {
-      RedHat:{
-        file { "/tmp/${check_mk_agent}":
-          ensure => present,
-          path   => "/tmp/${check_mk_agent}",
-          source => "puppet:///modules/check_mk/${check_mk_agent}"
-        }
-
-        exec { "rpm -i /tmp/${check_mk_agent}":
-          cwd     => '/tmp',
-          creates => '/usr/bin/check_mk_agent',
-#          require => Package['xinetd']
-        }
-      }
+      RedHat:{ package { 'check_mk': ensure => latest}}
       Debian:{ package { 'check-mk-agent': ensure => latest}}
       default: {notify{'OS not support for check_mk agent':}}
     }
